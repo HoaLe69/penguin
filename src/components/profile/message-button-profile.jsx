dd9d5a36edffd6aa1e008718aa10635a7d246062
Try More <hoabanhanh@gmail.com>
2024-11-19 20:20:48 +0700

    feat(profile): add loading state button message

--- src/components/profile/message-button-profile.jsx
+++ src/components/profile/message-button-profile.jsx
@@ -1,20 +1,22 @@
-import { Button } from '@chakra-ui/react'
+import { Button, Spinner, useToast } from '@chakra-ui/react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import axiosClient from '../../config/axios'
 import { getCurrentSelectedRoom } from '../../redux/conversationSlice'
-import { useToast } from '@chakra-ui/react'
 
 const GotoChatButton = ({ member, receiver }) => {
   const toast = useToast()
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const [senderId, receiveId] = member
 
   const handleGoToRoomChat = async () => {
-    if (senderId && receiveId) {
-      try {
+    try {
+      if (senderId && receiveId) {
+        setLoading(true)
         const room = await axiosClient.get(`/conversation/find/${senderId}/${receiveId}`)
         if (room?.id) {
           dispatch(getCurrentSelectedRoom({ info: room, receiver }))
@@ -23,18 +25,22 @@ const GotoChatButton = ({ member, receiver }) => {
           dispatch(getCurrentSelectedRoom({ info: res, receiver }))
         }
         navigate('/chat')
-      } catch (err) {
-        toast({
-          position: 'bottom-right',
-          title: 'Message',
-          description: 'Something went wrong',
-          duration: 1500
-        })
-        console.log(err)
+      } else {
+        throw new Error('Something went wrong')
       }
+    } catch (err) {
+      toast({
+        position: 'bottom-right',
+        title: 'Message',
+        description: 'Something went wrong',
+        duration: 1500
+      })
+      console.log(err)
+    } finally {
+      setLoading(false)
     }
   }
-  return <Button onClick={handleGoToRoomChat}>Messages</Button>
+  return <Button onClick={handleGoToRoomChat}>{loading ? <Spinner /> : 'Messages'}</Button>
 }
 
 export default GotoChatButton
