08bd4414a8b5d6da8322f7912eb9be88edf70499
Try More <hoabanhanh@gmail.com>
2024-11-20 15:31:47 +0700

    refactor: hidden the chat float on the small screen

--- src/components/chat-float/chat-float.jsx
+++ src/components/chat-float/chat-float.jsx
@@ -7,7 +7,7 @@ const ChatFloat = () => {
   const rooms = useSelector(state => state.room.roomFloatSelect.rooms)
 
   return (
-    <Box position="fixed" display="flex" gap={5} bottom={0} right={10}>
+    <Box position="fixed" display={{ base: 'none', lg: 'flex' }} gap={5} bottom={0} right={10}>
       {rooms?.map((room, index) => {
         return <ChatFloatRoom key={room?.info?.id || index} room={room.info} receiver={room?.receiver} />
       })}
